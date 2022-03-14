fa8f479264f14373954527f7b143f23c91297890
lyleokoth <okothlyleochieng@gmail.com>
2022-03-14 12:50:19 +0300

    Created the first test

---
+++ tests/test_home.py
@@ -0,0 +1,10 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    return app.test_client()
+
+def test_home(client):
+    resp = client.get('/api')
+    assert resp.status_code == 200
