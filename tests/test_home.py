5cef4c61e57c53c3978c3791b89417236a80b70a
lyleokoth <okothlyleochieng@gmail.com>
2022-03-14 13:37:57 +0300

    fix(test_home.py): Corrected flake8 errors.

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,10 +1,13 @@
 import pytest
 from app import app
 
+
 @pytest.fixture
 def client():
     return app.test_client()
 
+
 def test_home(client):
     resp = client.get('/api')
     assert resp.status_code == 200
+
