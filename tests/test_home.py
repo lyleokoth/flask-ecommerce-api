53c0b4e80a190e0c30217011d5b27797715a3961
lyleokoth <okothlyleochieng@gmail.com>
2022-03-14 14:27:02 +0300

    feat(test_home.py): added test for POST method.

--- tests/test_home.py
+++ tests/test_home.py
@@ -11,3 +11,8 @@ def client():
 def test_home(client):
     resp = client.get('/api')
     assert resp.status_code == 200
+
+
+def test_home_bad_http_method(client):
+    resp = client.post('/api')
+    assert resp.status_code == 405
